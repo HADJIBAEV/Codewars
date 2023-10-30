325952cb7ab2ed066099e3ff47792de12c50c3d3
Kahramon <hadjibaevkahramon@gmail.com>
2023-10-30 13:32:08 -0700

    Addition file

---
+++ Level 8kyu/MakeFunctionThatDoesArithmetic.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Arithmetic(a int, b int, operator string) int {
+	switch {
+	case strings.ToLower(operator) == "add":
+		return a + b
+	case strings.ToLower(operator) == "subtract":
+		return a - b
+	case strings.ToLower(operator) == "multiply":
+		return a * b
+	case strings.ToLower(operator) == "divide":
+		return a / b
+	default:
+		return 0
+	}
+
+}
+func main() {
+	fmt.Println(strings.ToLower("ADD"))
+	fmt.Println(Arithmetic(5, 2, "Add"))
+}
